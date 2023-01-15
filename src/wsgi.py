863ea1b49afc9407976ffc6c00f1d61c98b2ac25
Zhachory Volker <zhachory1@outlook.com>
2023-01-15 00:13:55 +0000

    Added todo

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,4 +1,5 @@
 from src import create_app
 
+# TODO(zhach): Add flag to switch between dev, prod, and test
 if __name__ == "__main__":
     create_app("src.conf.ProductionConfig").run(host="0.0.0.0")
