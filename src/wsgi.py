469069c45a1c7a9daf4d904f8990b44464d70b23
Zhachory Volker <zhachory1@outlook.com>
2023-01-15 00:13:16 +0000

    Default config to production, will add flags to switch between later

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,4 +1,4 @@
 from src import create_app
 
 if __name__ == "__main__":
-    create_app("src.conf.DevelopmentConfig").run(host="0.0.0.0")
+    create_app("src.conf.ProductionConfig").run(host="0.0.0.0")
